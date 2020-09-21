b40564e2459c78cd12a54c0bc9c23539cbd323e3
Mick Vleeshouwer <mick@imick.nl>
2020-09-21 14:20:48 +0200

    Cancel task on unload entry
    Signed-off-by: Mick Vleeshouwer <mick@imick.nl>

--- custom_components/tahoma/__init__.py
+++ custom_components/tahoma/__init__.py
@@ -201,6 +201,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
 
     hass.data[DOMAIN][entry.entry_id]["update_listener"]()
+    hass.data[DOMAIN][entry.entry_id]["task_refresh_state"]()
     entities_per_platform = hass.data[DOMAIN][entry.entry_id]["entities"]
 
     unload_ok = all(
